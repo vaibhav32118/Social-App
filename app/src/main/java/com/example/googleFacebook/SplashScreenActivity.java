58270ba2cce37c5ec2a82ae344e4e5061261205d
VAIBHAV32118 <vaibhavkapse32@gmail.com>
2021-05-20 13:12:13 +0530

    uploded

---
+++ app/src/main/java/com/example/googleFacebook/SplashScreenActivity.java
@@ -0,0 +1,47 @@
+package com.example.googleFacebook;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends Activity {
+
+    TextView designed, name, app_name;
+    ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        logo = findViewById(R.id.logo);
+        designed = findViewById(R.id.designed);
+        name = findViewById(R.id.name);
+//        app_name = findViewById(R.id.app_name);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startEnterAnimation();
+            }
+        }, 500);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2500);
+    }
+
+    private void startEnterAnimation() {
+        logo.setVisibility(View.VISIBLE);
+        designed.setVisibility(View.VISIBLE);
+        name.setVisibility(View.VISIBLE);
+    }
+}
